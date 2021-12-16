c1da14719d7edacca8592f59e864a4fa93f6a1cf
Gabriel Souza <gabrielsouza849@gmail.com>
2021-12-15 23:38:07 -0300

    mexendo tela

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -12,58 +12,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('web crawller'),
+        ),
+        body: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'url'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'nome das imagens '),
+            ),
+            TextButton(
+              child: const Text("runner"),
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(16.0),
+                primary: Colors.blueAccent,
+                textStyle: const TextStyle(fontSize: 30),
+              ),
+              onPressed: null,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'a',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
