d5ff82e29da84197e3aff0171f402184dcf14ca6
Gabriel Souza <gabrielsouza849@gmail.com>
2021-12-29 23:34:25 -0300

    adicionando imagen no click

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,73 +1,69 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(title: 'webcrawller', home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
 
+class HomeState extends State<MyApp> {
   // This widget is the root of your application.
+  List<Widget> listImages = [];
+
   @override
   Widget build(BuildContext context) {
     debugPrint(">>>>>>>>>>>>> teste wid >>>>>>>>>>>>>");
-    log(">>>>>>>>>>>>> teste wid >>>>>>>>>>>>>");
-    List<Widget> listImages = [
-      FadeInImage.memoryNetwork(
-        placeholder: kTransparentImage,
-        image: 'https://picsum.photos/250?image=9',
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('web crawller'),
       ),
-      FadeInImage.memoryNetwork(
-        placeholder: kTransparentImage,
-        image: 'https://picsum.photos/250?image=9',
-      )
-    ];
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('web crawller'),
-        ),
-        body: Column(
-          children: [
-            Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'url'),
+      body: Column(
+        children: [
+          Form(
+              child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'url'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'name imagens'),
+              ),
+              TextButton(
+                child: const Text("runner"),
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(16.0),
+                  primary: Colors.blueAccent,
+                  textStyle: const TextStyle(fontSize: 30),
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'name imagens'),
-                ),
-                TextButton(
-                  child: const Text("runner"),
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.blueAccent,
-                    textStyle: const TextStyle(fontSize: 30),
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            )),
-            Container(
-                height: 250,
-                padding: const EdgeInsets.all(5),
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: listImages.length,
-                    itemBuilder: (BuildContext ctx, int index) {
-                      debugPrint(">>>>>>>>>>>>> teste >>>>>>>>>>>>>${index}");
-                      return FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image: 'https://picsum.photos/250?image=9',
-                      );
-                    }))
-          ],
-        ),
+                onPressed: () {
+                  debugPrint(">>>>>>>>>>>>> press teste >>>>>>>>>>>>>");
+                  listImages.add(FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: 'https://picsum.photos/250?image=9',
+                  ));
+                  debugPrint(" >>>>>>> lista tamanho ${listImages.length}");
+                  setState(() {});
+                },
+              ),
+            ],
+          )),
+          Container(
+              height: 250,
+              padding: const EdgeInsets.all(5),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: listImages.length,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    debugPrint(">>>>>>>>>>>>> teste >>>>>>>>>>>>>${index}");
+
+                    return listImages[index];
+                  }))
+        ],
       ),
     );
   }
